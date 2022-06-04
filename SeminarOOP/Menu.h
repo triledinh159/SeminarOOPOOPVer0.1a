a2dbb702ec03be2ca1129a7cb3118ced20ea42a8
triledinh159 <91708260+triledinh159@users.noreply.github.com>
2022-06-05 00:49:59 +0700

    Add project files.

---
+++ SeminarOOP/Menu.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "SelectionSort.h"
+#include "InterchangeSort.h"
+#include "InsertionSort.h"
+#include "BinaryInsertionSort.h"
+#include "BubbleSort.h"
+#include "ShakerSort.h"
+#include "ShellSort.h"
+#include "RadixSort.h"
+#include "CountingSort.h"
+#include "HeapSort.h"
+#include "QuickSort.h"
+#include "MergeSort.h"
+#include "Coordinate.h"
+//#include <thread>
+void check(char&); // in hướng dẫn cho người dùng và kiểm tra người dùng muốn tiếp tục hay thoát
+
+void selection(int); // xử lý các lựa chọn thuật toán của người dùng
+
+void infor(); // thông tin nhóm thực hiện
+ 
+void command(); // điều khiển chương trình
+
+
